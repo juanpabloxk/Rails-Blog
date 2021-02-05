989761496687746078706d6539f67df03f4ff772
Newer Coder <xleed@protonmail.ch>
2021-02-04 21:09:07 -0500

    migrations modified

--- db/migrate/20210203185129_create_articles.rb
+++ db/migrate/20210203185129_create_articles.rb
@@ -1,5 +1,6 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
+    
     create_table :articles do |t|
       t.string :title
       t.text :body
--- db/migrate/20210204151556_create_categories.rb
+++ db/migrate/20210204151556_create_categories.rb
@@ -3,7 +3,6 @@ class CreateCategories < ActiveRecord::Migration[6.1]
     create_table :categories do |t|
       t.string :name
       t.text :description
-
       t.timestamps
     end
   end
