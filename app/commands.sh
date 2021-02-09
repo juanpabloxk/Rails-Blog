ff5bdababdf532bf9f30ebe8a0f0c17726ca83d4
Newer Coder <xleed@protonmail.ch>
2021-02-08 23:48:50 -0500

    auth implementation started, created user and session controllers, installed bcrypt

---
+++ app/commands.sh
@@ -0,0 +1,3 @@
+bin/rails generate model User name:string password_digest:string
+rails g controller sessions new create login welcome
+gem install bcrypt
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
   root "articles#index"
   resources :articles
   resources :categories do
