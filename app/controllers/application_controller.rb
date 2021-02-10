4a9df86c4d97ecb02de88de4ad8d6688f91b3a6e
Newer Coder <xleed@protonmail.ch>
2021-02-09 19:24:26 -0500

    added categories overview to wellcome view (with some db fetch optimizations)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,24 @@ class ApplicationController < ActionController::Base
     before_action :authorized
     helper_method :current_user
     helper_method :logged_in?
+    helper_method :cached_categories
 
     def current_user    
         User.find_by(id: session[:user_id])  
     end
     
     def logged_in?
-        !current_user.nil?
+        !session[:user_id].nil? && !current_user.nil?
     end
     
     def authorized
         redirect_to '/unauthorized' unless logged_in?
+    end
+
+    def cached_categories
+        unless defined? @categories
+            @categories = Category.all.map { |c| c.name }
+        end
+        @categories
     end
 end
