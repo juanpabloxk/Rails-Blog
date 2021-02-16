cfd976faa4a32ddc589748293bd141ee43d4551f
Newer Coder <xleed@protonmail.ch>
2021-02-16 17:43:55 -0500

    modified a migration to avoid deploy issues

--- db/migrate/20210210203700_add_user_to_arcicle_and_comments.rb
+++ db/migrate/20210210203700_add_user_to_arcicle_and_comments.rb
@@ -1,6 +1,6 @@
 class AddUserToArcicleAndComments < ActiveRecord::Migration[6.1]
   def change
-    add_reference :articles, :user, null: false, foreign_key: true, default: 1
-    add_reference :comments, :user, null: false, foreign_key: true, default: 1
+    add_reference :articles, :user, null: false, foreign_key: true
+    add_reference :comments, :user, null: false, foreign_key: true
   end
 end
