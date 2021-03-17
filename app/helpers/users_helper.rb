5a1131e012238628ac56a84b75a72c551a0175d2
Newer Coder <xleed@protonmail.ch>
2021-03-16 19:33:00 -0500

    user graph is now passed as svg string to view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -13,6 +13,7 @@ module UsersHelper
   def self.create_user_graph(user)
     g = GraphViz.new( :G, :type => :digraph )
     user_node = g.add_nodes("#{user.username}")
+    user_node.set { |_n| _n.color = "#1976D2" }
     user.followers.each do |follower|
       follower_node = g.add_nodes("#{follower.username}")
       g.add_edges( follower_node, user_node )
@@ -23,7 +24,6 @@ module UsersHelper
     end
     file = Tempfile.new(['userimage','.svg'])
     g.output( :svg => file.path )
-    FileUtils.mv(file.path, UsersHelper.internal_user_graph_path)
-    File.basename(file.path)
+    file.read.html_safe
   end
 end
